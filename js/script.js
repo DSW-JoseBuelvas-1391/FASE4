9b63a84ced290512015c0375e3b78f98e336acc8
DSW-JoseBuelvas-1391 <125063753+DSW-JoseBuelvas-1391@users.noreply.github.com>
2023-05-12 10:28:14 -0500

    Update script.js
    actualizacion 7

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 function procesarRespuestas() {
-    let total=3;
-    let puntos=0;
+    let total = 3;
+    let puntos = 0;
     let myForm = document.forms["formulario"];
-    let respuestasCorrectas= ["c","a","b"];
-    for (let i=1;i <=total;i++){
-        if (myForm["r"+ i].value== null||
-            myForm["r"+ i].value== '' ){
-            alert ('Favor responder todas las preguntas');
-            return false;    
-            } else {
-                if (myForm ["r+i"].value === respuestasCorrectas[i - 1])
-                puntos++;
+    let respuestasCorrectas = ["c", "a", "b"];
+    for (let i = 1; i <= total; i++) {
+      // Se debe utilizar la comparación estricta (triple igual) en lugar de la comparación débil (doble igual).
+      if (myForm["r" + i].value === null || myForm["r" + i].value === "") {
+        alert("Favor responder todas las preguntas");
+        return false;
+      } else {
+        // Se debe utilizar el operador de concatenación (+) para construir el nombre de la variable.
+        if (myForm["r" + i].value === respuestasCorrectas[i - 1]) {
+          puntos++;
         }
+      }
     }
-    let resultado = document.getElementById('resultado')
-    resultado.innerHTML='Obtuviste'+puntos+'puntos de'+ total+' posibles';
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = "Obtuviste " + puntos + " puntos de " + total + " posibles";
     return false;
-}
+  }
